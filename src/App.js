42027814c7afe4c884d76f1f6bb488f98c049aa4
tanverified <khantanveer2u@gmail.com>
2021-10-17 09:29:13 +0530

    yesterdays

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+import React from 'react';
+import './App.css';
+// import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header/> */}
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
