d1e934fd933a336c4eb0a1f48a6740499e3b8a91
tanverified <khantanveer2u@gmail.com>
2021-10-17 11:35:11 +0530

    sections added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,57 @@
-import React from 'react'
-import styled from 'styled-components';
-import Section from './Section'
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
 
 function Home() {
-    return (
-        <Container>
-            <Section/>
-        </Container>
-    )
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Lowest cost solar panels in America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Produce Clean Energy From Your Roof"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop Now"
+      />
+    </Container>
+  );
 }
 
 export default Home;
 
 const Container = styled.div`
-    height: 100vh;
-`
+  height: 100vh;
+`;
